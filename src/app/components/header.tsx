61daea7f5fc11ff75ba12d39fc2c29ecd7b34b43
DitchTurtL <ridium@gmail.com>
2024-03-17 03:32:09 +0000

    Adding Projects layout and initial content

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
                 href="#">Docs</a>
 
                 <a className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" 
-                href="#">Projects</a>
+                href="/projects">Projects</a>
 
                 <a className="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" 
                 href="#">About</a>
