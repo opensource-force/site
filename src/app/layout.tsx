53bf59aaafcef48ec13a4a78d5d8fc19027513ef
akpi816218 <gh@akpi.is-a.dev>
2024-04-04 16:57:24 -0700

    Upgrade deps, format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Header  from "./components/header";
-import "./globals.css";
-import "./devIcons.min.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Header from './components/header';
+import './globals.css';
+import './devIcons.min.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Open Source Force",
-  description: "An OSF Website.",
+	title: 'Open Source Force',
+	description: 'An OSF Website.'
 };
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="bg-slate-50 dark:bg-slate-900">
-        <Header/>
-        {children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className="bg-slate-50 dark:bg-slate-900">
+				<Header />
+				{children}
+			</body>
+		</html>
+	);
 }
