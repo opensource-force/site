5a415c62e186af7e6d1488b55f3e92c644d5f213
akpi816218 <gh@akpi.is-a.dev>
2024-04-04 17:17:22 -0700

    delete favicon, refactor, fix SVG prop names

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,65 @@ import './devIcons.min.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: 'Open Source Force',
-	description: 'An OSF Website.'
+	title: { default: 'Open Source Force', template: '%s | Open Source Force' },
+	description:
+		'A community of open source enthusiasts working together to innovate and inspire.',
+	generator: 'Next.js',
+	referrer: 'origin-when-cross-origin',
+	keywords: [
+		'open source',
+		'community',
+		'innovation',
+		'inspiration',
+		'code',
+		'development',
+		'devs',
+		'developers',
+		'discord'
+	],
+	authors: [
+		{
+			name: 'Akhil Pillai',
+			url: 'https://akpi.is-a.dev/'
+		}
+	],
+	creator: 'Open Source Force',
+	publisher: 'Open Source Force',
+	// formatDetection: {
+	// 	address: false,
+	// 	date: false,
+	// 	email: false,
+	// 	telephone: false,
+	// 	url: false
+	// },
+	openGraph: {
+		title: 'Open Source Force',
+		description:
+			'A community of open source enthusiasts working together to innovate and inspire.',
+		url: 'https://opensourceforce.net/',
+		type: 'website',
+		locale: 'en_US'
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			noimageindex: false
+		}
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Open Source Force',
+		description:
+			'A community of open source enthusiasts working together to innovate and inspire.'
+		// images: ['https://opensourceforce.net/twitter-image.png']
+	},
+	alternates: {
+		canonical: 'https://opensourceforce.net/'
+	},
+	category: ''
 };
 
 export default function RootLayout({
